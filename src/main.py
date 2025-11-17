698feb9d8685fa168b69eef28ffa1a1e75a98ea1
mafaldacvcardo <mafaldacvc@gmail.com>
2025-11-17 02:25:06 +0100

    Delete src directory

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import utils.utils as utils
-
-def main():
-    print("FARPE DATA ANALYSIS")
-    
-    print("All analyses have been executed successfully")
-
-if __name__ == "__main__":
-    main()
